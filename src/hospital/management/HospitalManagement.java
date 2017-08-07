ff68bc84d43071a84da6ad9e24f3395145fe0f58
Sayef Reyadh <sayef.reyadh@gmail.com>
2017-08-07 12:47:46 +0600

    Hospital Management System
    Hospital Management System
    
    Database Lab Project
    CSE3104
    AUST

---
+++ src/hospital/management/HospitalManagement.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hospital.management;
+
+import hospital.management.design.Login;
+
+/**
+ *
+ * @author SayefReyadh
+ */
+public class HospitalManagement {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Login().setVisible(true);
+    }
+    
+}
